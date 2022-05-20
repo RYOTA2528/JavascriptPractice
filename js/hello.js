806a0d6ff433ae02698b5f85a40f66b4e1f2b682
RYOTA2528 <1216022@gmail.com>
2022-05-20 23:46:26 +0900

    【ADD】Javascriptの学習を更新

---
+++ js/hello.js
@@ -0,0 +1,2 @@
+console.log('Hello World! from hello.js');
+////GitPush
---
+++ js/valiable.js
@@ -0,0 +1,33 @@
+//変数について
+
+//変数の宣言
+//var 変数名　[=初期値]
+
+//変数xを宣言
+var x;
+
+//複数の変数を宣言
+//var i;
+//var j;
+//または
+var i,j;
+
+//宣言時に初期値をセットできる
+var str = 'はじめてのJavascript'
+var k = 100;
+
+//命令規則
+//１文字目は英字orアンダースコアor数字
+var name;
+var _name;
+var $name;
+
+//大文字小文字は区別される。
+var Foo;
+var foo;
+
+//例題
+var colorName = 'blue'
+console.log(colorName);
+
+////GitPush
