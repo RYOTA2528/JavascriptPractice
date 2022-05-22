3eb07b59fb5dba86f773ac9a76c839c7f689fbbc
RYOTA2528 <1216022@gmail.com>
2022-05-23 00:14:26 +0900

    【ADD】Javascriptの学習（繰り返し処理while文）

--- js/loop.js
+++ js/loop.js
@@ -5,3 +5,13 @@
 for ( var i = 1; i <= 3; i++){
   console.log(i);
 }
+
+//例題while
+//変数の値を1から3まで出力するプログラムを、
+//while文を使って記述
+//(補足)while文は条件を満たすまで行う処理
+var j = 1;
+while(j <= 3) {
+  console.log(j);
+  j++;
+}
