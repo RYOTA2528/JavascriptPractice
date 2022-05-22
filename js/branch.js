84f8168a59566a899ca3a02867d819e2e152bfe0
RYOTA2528 <1216022@gmail.com>
2022-05-22 23:24:10 +0900

    【ADD】Javascriptの学習を更新(比較演算子(else if))

--- js/branch.js
+++ js/branch.js
@@ -9,3 +9,19 @@ if (x >= 100) {
 } else {
   console.log('100未満の値です');
 }
+
+
+//if  /else if /else
+//テストの点数腕の出力
+//90点以上A,80点以上B,60点以上C,60点未満D
+var y = 90;
+if(y <= 90){
+  console.log('A')
+}
+else if(y >= 80){
+  consol.log('B')
+} else if(y >= 60) {
+  console.log('C')
+} else {
+  console.log('D');
+}
